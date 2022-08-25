ec47625ca4ecd483cf5800038c23458d4704dfe5
DeboraMCosta <89197458+DeboraMCosta@users.noreply.github.com>
2022-08-25 20:16:04 -0300

    C++

---
+++ EXERCISE.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    
+    double area;
+    double raio;
+    cout << setprecision(4) << fixed;
+    cin >> raio;
+    area = pow(raio,2) * 3.14159;
+    cout << "A=" << area;
+    
+    return 0;
+}
